d15fd9a34e6a2defb4606c652e8f2881791f22ff
Markus de Medeiros <markusdemedeiros@outlook.com>
2024-12-25 15:27:59 -0700

    separate files

--- coqdep.lean
+++ coqdep.lean
@@ -39,7 +39,6 @@ def parseFile (l : String) : RocqFile :=
 
 abbrev DepGraph := Lean.HashSet (RocqFile × RocqFile)
 
-
 def parse_graph (l : String)  :=
   let l_sep := String.split l (BEq.beq ':')
   let l_of := String.split l_sep[0]! (BEq.beq ' ')
@@ -51,16 +50,20 @@ def parse_graph (l : String)  :=
 instance : ToString DepGraph where
   toString x := List.foldl (fun acc s => s!"{acc}\n{s.1} -> {s.2}") "" x.toList
 
-
-
--- def depgraph.nodes (d : DepGraph) : List RocqFile :=
---   (d.filter (fun g => g.1.type == "v" && g.2.type == "v")).map (fun g => g.1)
-
-
+-- String representation of a path
 def dbg_path (x : List String) : String :=
   (List.foldl (fun acc s => s!"{acc}_{s}") "" x).drop 1
 
+-- String representation of a ndoe
 def dbg_node (x : RocqFile) : String := s!"{dbg_path x.1}_{x.2}_{x.3}"
+
+
+-- String representation of a path
+def show_path (x : List String) : String :=
+  (List.foldl (fun acc s => s!"{acc}/{s}") "" x).drop 1
+
+-- String representation of a ndoe
+def show_node (x : RocqFile) : String := s!"{x.2}.{x.3}"
 
 --
 -- def depgraph.from_vo (d : RocqFile × RocqFile) : Bool := d.1.type == "vo"
@@ -111,37 +114,42 @@ def to_concrete (n : RocqFile) : RocqFile := ⟨ n.path, n.type, "v" ⟩
 def depgraph.to_graph (g : DepGraph) : IO String := do
   let n := g.nodes
   let p := all_paths n
-
   let render_subgraph (s : List String) : String :=
     let content : String := n.fold
-      (fun acc nod => if (nod.1 == s) then s!"{acc}{dbg_node nod};\n" else acc) ""
+      (fun acc nod => if (nod.1 == s) then s!"{acc}{dbg_node nod}[label=\"{show_node nod}\"];\n" else acc) ""
     -- s!"subgraph cluster_{dbg_path s} \{\n{dbg_path s}\n{content}}\n"
     s!"subgraph cluster_{dbg_path s} \{\n{content}}\n"
   let subgraphs : String := p.fold (fun acc s => s!"{acc}{render_subgraph s}") ""
-
   let render_edge (d : (RocqFile × RocqFile)) : String :=
     if (d.1.path == d.2.path)
       then s!"{dbg_node d.1} -> {dbg_node d.2};\n" -- render the within-cluster edges
       else ""
   let edges : String := g.fold (fun acc s => s!"{acc}{render_edge s}") ""
+  return s!"digraph \{\ncompound=true;\noverlap=false;\n{subgraphs}\n{edges}\n}\n"
 
+
+
+def depgraph.to_overview_graph (g : DepGraph) : IO String := do
+  let n := g.nodes
+  let p := all_paths n
   -- Calculate inter-cluster edges
   let g_ic_edges : Lean.HashSet (List String × List String) :=
     g.fold
       (fun acc s =>
-        if s.1 == s.2 then acc else
+        if s.1 == s.2 && s.1.1 != s.2.1 then acc else
         acc.insert (s.1.1, s.2.1))
       Lean.HashSet.empty
   let render_ic_edge (d : List String × List String) : String :=
-    s!"{dbg_path d.1} -> {dbg_path d.2} [ltail=cluster_{dbg_path d.1}, lhead=cluster_{dbg_path d.2}, weight=0.1] ;\n"
+    let dp1 := dbg_path d.1
+    let dp2 := dbg_path d.2
+    if dp1 != dp2 then s!"{dp1} -> {dp2};\n" else ""
   let ic_edges : String := g_ic_edges.fold (fun acc s => s!"{acc}{render_ic_edge s}") ""
-
   let overview : String :=
-    let content : String := p.fold (fun acc pat => s!"{acc}{dbg_path pat};\n" ) ""
+    let content : String := p.fold (fun acc pat => s!"{acc}{dbg_path pat}[label=\"{show_path pat}\"];\n" ) ""
     -- s!"subgraph cluster_{dbg_path s} \{\n{dbg_path s}\n{content}}\n"
     s!"subgraph overview \{\n{content}}\n"
+  return s!"digraph \{\ncompound=true;\noverlap=false;\n{overview}\n{ic_edges}\n}\n"
 
-  return s!"digraph \{\ncompound=true;\noverlap=false;\n{overview}\n{ic_edges}\n{subgraphs}\n{edges}\n}\n"
 
 
 def main (args : List String) : IO Unit := do
@@ -151,30 +159,32 @@ def main (args : List String) : IO Unit := do
     | none => throw <| IO.userError "please provide the path of _CoqProject as first argument"
   let g : DepGraph := Lean.HashSet.insertMany Lean.HashSet.empty <| (<- IO.FS.lines path).toList.bind parse_graph
 
+
+
   -- Simplify g by
   --    - removing self-edges
   --    - collapsing all X.* nodes into X
   --    - if
   let g2 : DepGraph :=
     g.fold (fun acc s =>
-      if (s.1.path != s.2.path)
-        then acc.insert (⟨ s.1.path, s.1.name, "v" ⟩, ⟨ s.2.path, s.2.name, "v" ⟩)
-        else if s.1.name == s.2.name
-          then acc -- acc.insert (to_concrete s.1, to_concrete s.2) -- Edge within cluster
-          else acc.insert (⟨ s.1.path, s.1.name, "v" ⟩, ⟨ s.2.path, s.2.name, "v" ⟩)
+      if (s.1.path = s.2.path) && (s.1.name == s.2.name)
+        then acc
+        else acc.insert (⟨ s.1.path, s.1.name, "v" ⟩, ⟨ s.2.path, s.2.name, "v" ⟩)
     )
     Lean.HashSet.empty
 
-  -- -- Simplify the graph
-  -- let pt := g.points_to
-  -- let n := g.nodes
+
+  -- Write out the individual graphs into files
+  let g2_nodes := g.nodes
+  for i in all_paths g2_nodes do
+    let i_graph := g2.fold (fun acc s => if (s.1.path == i) && (s.2.path == i) then acc.insert s else acc) Lean.HashSet.empty
+    IO.FS.writeFile s!"./dot/{dbg_path i}.dot" (<- depgraph.to_graph i_graph)
+
+  -- Write out the overview graph
+  IO.FS.writeFile s!"./dot/overview.dot" (<- depgraph.to_overview_graph g2)
 
 
-  -- for nod in n do
-  --   IO.println s!"{nod}"
-    -- IO.println s!"{(concrete_descendents pt nod).length}\n"
-
-  IO.FS.writeFile "./out.dot" (<- depgraph.to_graph g2)
+  -- IO.FS.writeFile "./out.dot" (<- depgraph.to_graph g2)
   IO.println "done"
 
 
